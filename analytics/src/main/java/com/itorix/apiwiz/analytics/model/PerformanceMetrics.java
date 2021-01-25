f44888ae22a3ce8480473c14db532340fd555ac7
Sudhakar <sudhakar.alaparthi@itorix.com>
2021-01-25 11:28:54 +0530

    Adding analytics module

---
+++ analytics/src/main/java/com/itorix/apiwiz/analytics/model/PerformanceMetrics.java
@@ -0,0 +1,83 @@
+package com.itorix.apiwiz.analytics.model;
+
+public class PerformanceMetrics {
+
+	
+	private String orgName;
+	
+	private String environment;
+	
+	private String timeRange;
+	
+	private String timeUnit;
+
+	private String appName;
+	
+	private String productName;
+	
+	private String type;
+	
+	
+	
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getAppName() {
+		return appName;
+	}
+
+	public void setAppName(String appName) {
+		this.appName = appName;
+	}
+
+	public String getOrgName() {
+		return orgName;
+	}
+
+	public void setOrgName(String orgName) {
+		this.orgName = orgName;
+	}
+
+	public String getEnvironment() {
+		return environment;
+	}
+
+	public void setEnvironment(String environment) {
+		this.environment = environment;
+	}
+
+	public String getTimeRange() {
+		return timeRange;
+	}
+
+	public void setTimeRange(String timeRange) {
+		this.timeRange = timeRange;
+	}
+
+	public String getTimeUnit() {
+		return timeUnit;
+	}
+
+	public void setTimeUnit(String timeUnit) {
+		this.timeUnit = timeUnit;
+	}
+	
+	
+	
+	
+	
+	
+}
