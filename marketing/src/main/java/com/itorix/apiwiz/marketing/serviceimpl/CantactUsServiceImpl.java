f86747545ed512060bf104c171340dadb3830a15
Sudhakar <sudhakar.alaparthi@itorix.com>
2021-02-02 17:23:16 +0530

    making jfrog port optional in properties

--- marketing/src/main/java/com/itorix/apiwiz/marketing/serviceimpl/CantactUsServiceImpl.java
+++ marketing/src/main/java/com/itorix/apiwiz/marketing/serviceimpl/CantactUsServiceImpl.java
@@ -3,11 +3,15 @@ package com.itorix.apiwiz.marketing.serviceimpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.itorix.apiwiz.marketing.contactus.model.ContactUsNotification;
 import com.itorix.apiwiz.marketing.dao.ContactUsDao;
 import com.itorix.apiwiz.marketing.service.CantactUsService;
 
+@CrossOrigin
+@RestController
 public class CantactUsServiceImpl implements CantactUsService {
 
 	@Autowired
