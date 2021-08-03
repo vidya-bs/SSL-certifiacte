8452f1db3179ff9502032d7d0c444d8b8e3141fa
balajivijayan <balaji@itorix.com>
2021-08-03 18:02:18 +0530

    EN576 - upgraded test-suite lib to 2.x

--- test-suite-lib/src/main/java/com/itorix/apiwiz/test/config/MicrometerConfiguration.java
+++ test-suite-lib/src/main/java/com/itorix/apiwiz/test/config/MicrometerConfiguration.java
@@ -1,16 +1,14 @@
 package com.itorix.apiwiz.test.config;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.spring.autoconfigure.MeterRegistryCustomizer;
 
 @Configuration
 public class MicrometerConfiguration {
 
-	@Bean
-	MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+	@Bean MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
 		return registry -> registry.config();
 	}
 }
