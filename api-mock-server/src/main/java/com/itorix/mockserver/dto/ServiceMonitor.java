63e9dfdab353790a8a24728c6b97a0b28cfae72f
balajivijayan <balaji@itorix.com>
2021-08-17 19:07:30 +0530

    EN-537 : added debug statement

--- api-mock-server/src/main/java/com/itorix/mockserver/dto/ServiceMonitor.java
+++ api-mock-server/src/main/java/com/itorix/mockserver/dto/ServiceMonitor.java
@@ -1,14 +1,13 @@
 package com.itorix.mockserver.dto;
 
-import java.lang.reflect.InvocationTargetException;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.itorix.mockserver.common.model.ErrorObj;
+import com.itorix.mockserver.common.model.ItorixException;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -16,13 +15,16 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Component;
 
-import com.itorix.mockserver.common.model.ErrorObj;
-import com.itorix.mockserver.common.model.ItorixException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.InvocationTargetException;
 
 @Aspect
 @Component
 public class ServiceMonitor {
+    public static Logger LOGGER = LoggerFactory.getLogger(ServiceMonitor.class);
     public static final String SESSION_TOKEN_NAME = "x-tenant";
+
     @Autowired
     private HttpServletRequest request;
     @Autowired
@@ -55,6 +57,7 @@ public class ServiceMonitor {
     private String populateTenant(JoinPoint thisJoinPoint)
             throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, ItorixException {
         String key = request.getHeader(SESSION_TOKEN_NAME);
+        LOGGER.info("Received Request from {} " , request.getRequestURI());
         if (key != null) {
             Workspace workspace = getWorkspaceWithKey(key);
             if (workspace != null) {
