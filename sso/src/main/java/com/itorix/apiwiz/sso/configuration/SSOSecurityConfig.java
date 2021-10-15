a7c04ac6ec458c60bf7611f44d2d5a1cc89dcfc8
balajivijayan <balaji@itorix.com>
2021-10-15 15:55:41 +0530

    EN-689 : removed cors config bean

--- sso/src/main/java/com/itorix/apiwiz/sso/configuration/SSOSecurityConfig.java
+++ sso/src/main/java/com/itorix/apiwiz/sso/configuration/SSOSecurityConfig.java
@@ -10,7 +10,6 @@ import org.apache.commons.httpclient.protocol.Protocol;
 import org.apache.commons.httpclient.protocol.ProtocolSocketFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -19,14 +18,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
-import java.util.Arrays;
-import java.util.Collections;
 
 @EnableWebSecurity
 @Configuration
@@ -110,27 +104,8 @@ public class SSOSecurityConfig {
                     .metadataFilePath(metadataUrl).and();
             http.exceptionHandling().accessDeniedHandler(accessDeniedHandler);
 
-            http.cors();
 
         }
-
-        @Bean
-        public CorsConfigurationSource corsConfigurationSource() {
-            final CorsConfiguration configuration = new CorsConfiguration();
-            configuration.setAllowedOrigins(Collections.unmodifiableList(Arrays.asList("*")));
-            configuration.setAllowedMethods(Collections.unmodifiableList(Arrays.asList("HEAD",
-                    "GET", "POST", "PUT", "DELETE", "PATCH")));
-            // setAllowCredentials(true) is important, otherwise:
-            // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
-            configuration.setAllowCredentials(true);
-            // setAllowedHeaders is important! Without it, OPTIONS preflight request
-            // will fail with 403 Invalid CORS request
-            configuration.setAllowedHeaders(Collections.unmodifiableList(Arrays.asList("Authorization", "Cache-Control", "Content-Type")));
-            final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-            source.registerCorsConfiguration("/**", configuration);
-            return source;
-        }
-
     }
 
     @Configuration
