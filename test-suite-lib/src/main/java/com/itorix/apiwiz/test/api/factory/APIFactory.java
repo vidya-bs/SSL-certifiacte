20aa5e7076aca2d3d1fe8c7563e54a5202d5fb83
AkashItorix <akash.k@itorix.com>
2022-08-01 10:49:56 +0530

    None Payload

--- test-suite-lib/src/main/java/com/itorix/apiwiz/test/api/factory/APIFactory.java
+++ test-suite-lib/src/main/java/com/itorix/apiwiz/test/api/factory/APIFactory.java
@@ -2,6 +2,7 @@ package com.itorix.apiwiz.test.api.factory;
 
 import com.itorix.apiwiz.test.executor.beans.HttpDeleteWithBody;
 import org.apache.http.Consts;
+import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
@@ -112,17 +113,26 @@ public class APIFactory {
             SSLConnectionSocketFactory sslConnectionFactory, int timeout) throws ClientProtocolException, IOException {
         HttpResponse response = null;
         HttpClient client = getClient(sslConnectionFactory, timeout);
-        HttpDeleteWithBody request = new HttpDeleteWithBody(url);
+        HttpRequestBase request=null;
+        if(null != body && !"".equalsIgnoreCase(body)){
+         request = new HttpDeleteWithBody(url);  
+         addBody(request, body);
+        }
+        else
+        {
+        	request=new HttpDelete(url);
+        }
         addHeaders(request, headers);
-        addBody(request, body);
+
         response = client.execute(request);
+        
         return response;
     }
 
-    private static void addBody(HttpDeleteWithBody request, String body) {
+    private static void addBody(HttpRequestBase request, String body) {
         if(null != body && !"".equalsIgnoreCase(body)) {
             StringEntity entity = new StringEntity(body, ContentType.APPLICATION_JSON);
-            request.setEntity(entity);
+            ((HttpEntityEnclosingRequestBase) request).setEntity(entity);
         }
     }
 
