56228736959f3bd25b0a2f3f5a4be9ea15b256cf
Adeeb Tahir Rahamathullah <adeeb.tr@itorix.com>
2022-09-23 13:19:48 +0530

    blogs upload to s3

---
+++ marketing/src/main/java/com/itorix/apiwiz/marketing/serviceimpl/WebsiteStorageServiceImp.java
@@ -0,0 +1,28 @@
+package com.itorix.apiwiz.marketing.serviceimpl;
+
+import com.itorix.apiwiz.common.model.integrations.Integration;
+import com.itorix.apiwiz.common.model.integrations.gcs.GcsIntegration;
+import com.itorix.apiwiz.common.util.gcs.GcsUtils;
+import com.itorix.apiwiz.common.util.s3.S3Utils;
+import com.itorix.apiwiz.marketing.service.WebsiteStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@CrossOrigin
+@RestController
+public class WebsiteStorageServiceImp  implements WebsiteStorageService {
+
+    @Autowired
+    S3Utils s3Utils;
+
+    @Override
+    public ResponseEntity<?> uploadImage(String interactionid, String jsessionid, MultipartFile file) throws Exception {
+        String downloadUri= s3Utils.uploadWebsiteResource(file.getName()+System.currentTimeMillis(),file.getInputStream());
+        return new ResponseEntity<>(downloadUri,HttpStatus.OK);
+    }
+}
