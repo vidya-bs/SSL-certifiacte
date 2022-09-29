1f00c0d8d9c76c70952cbecc302c6f6a4747f26d
rahul <rahul.b@itorix.com>
2022-09-29 09:28:46 +0530

    Notification-DD-Model

--- common/src/main/java/com/itorix/apiwiz/common/util/mail/MailUtil.java
+++ common/src/main/java/com/itorix/apiwiz/common/util/mail/MailUtil.java
@@ -1,5 +1,6 @@
 package com.itorix.apiwiz.common.util.mail;
 
+import com.itorix.apiwiz.common.model.exception.ItorixException;
 import com.itorix.apiwiz.common.properties.ApplicationProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,7 +11,10 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
-import java.nio.file.Path;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
@@ -27,73 +31,94 @@ public class MailUtil {
 	private EmailHelper emailHelper;
 
 	public void sendEmail(EmailTemplate emailTemplate) throws MessagingException {
-		try {
-			logger.debug("Initiating mail with subject {} to {} ", emailTemplate.getSubject(),
-					emailTemplate.getToMailId());
-			JavaMailSender javaMailSender = emailHelper.getJavaMailSender();
-			MimeMessage msg = javaMailSender.createMimeMessage();
-			MimeMessageHelper helper = new MimeMessageHelper(msg, true);
-			// SimpleMailMessage msg = new SimpleMailMessage();
-			String[] internetAddress = new String[emailTemplate.getToMailId().size()];
-			int i = 0;
-			for (String tomail : emailTemplate.getToMailId()) {
-				internetAddress[i] = tomail;
-				i++;
+
+		ExecutorService executor = Executors.newFixedThreadPool(12);
+
+		Future future = executor.submit(new Callable() {
+			@Override
+			public Object call() throws ItorixException, MessagingException {
+
+				try {
+					logger.debug("Initiating mail with subject {} to {} ", emailTemplate.getSubject(),
+							emailTemplate.getToMailId());
+					JavaMailSender javaMailSender = emailHelper.getJavaMailSender();
+					MimeMessage msg = javaMailSender.createMimeMessage();
+					MimeMessageHelper helper = new MimeMessageHelper(msg, true);
+					// SimpleMailMessage msg = new SimpleMailMessage();
+					String[] internetAddress = new String[emailTemplate.getToMailId().size()];
+					int i = 0;
+					for (String tomail : emailTemplate.getToMailId()) {
+						internetAddress[i] = tomail;
+						i++;
+					}
+					helper.setTo(internetAddress);
+					if (emailTemplate.getSubject() != null) {
+						helper.setSubject(emailTemplate.getSubject());
+					} else {
+						helper.setSubject(applicationProperties.getMailSubject());
+					}
+					helper.setFrom(emailHelper.getFromAddress());
+					helper.setText(emailTemplate.getBody(), true);
+					logger.info("Sending mail to with subject {} ", emailTemplate.getSubject());
+					javaMailSender.send(msg);
+					logger.info("Mail with subject {} sent successfully", emailTemplate.getSubject());
+				} catch (MessagingException e) {
+					logger.error(e.getMessage(), e);
+					throw e;
+				} catch (Exception e) {
+					logger.error(e.getMessage(), e);
+				}
+				return "Mail initiated";
 			}
-			helper.setTo(internetAddress);
-			if (emailTemplate.getSubject() != null) {
-				helper.setSubject(emailTemplate.getSubject());
-			} else {
-				helper.setSubject(applicationProperties.getMailSubject());
-			}
-			helper.setFrom(emailHelper.getFromAddress());
-			helper.setText(emailTemplate.getBody(), true);
-			logger.info("Sending mail to with subject {} ", emailTemplate.getSubject());
-			javaMailSender.send(msg);
-			logger.info("Mail with subject {} sent successfully", emailTemplate.getSubject());
-		} catch (MessagingException e) {
-			logger.error(e.getMessage(), e);
-			throw e;
-		} catch (Exception e) {
-			logger.error(e.getMessage(), e);
-		}
+		});
+
 	}
 
 	public void sendEmailWtithAttachment(EmailTemplate emailTemplate, String path, String attachment)
 			throws MessagingException {
-		try {
-			logger.debug("Initiating mail with subject {} to {} ", emailTemplate.getSubject(),
-					emailTemplate.getToMailId());
-			JavaMailSender javaMailSender = emailHelper.getJavaMailSender();
-			MimeMessage msg = javaMailSender.createMimeMessage();
-			MimeMessageHelper helper = new MimeMessageHelper(msg, true);
 
-			FileSystemResource file = new FileSystemResource(new File(path));
-			helper.addAttachment(attachment, file);
+		ExecutorService executor = Executors.newFixedThreadPool(12);
 
-			String[] internetAddress = new String[emailTemplate.getToMailId().size()];
-			int i = 0;
-			for (String tomail : emailTemplate.getToMailId()) {
-				internetAddress[i] = tomail;
-				i++;
+		Future future = executor.submit(new Callable() {
+			@Override
+			public Object call() throws ItorixException, MessagingException {
+
+				try {
+					logger.debug("Initiating mail with subject {} to {} ", emailTemplate.getSubject(),
+							emailTemplate.getToMailId());
+					JavaMailSender javaMailSender = emailHelper.getJavaMailSender();
+					MimeMessage msg = javaMailSender.createMimeMessage();
+					MimeMessageHelper helper = new MimeMessageHelper(msg, true);
+
+					FileSystemResource file = new FileSystemResource(new File(path));
+					helper.addAttachment(attachment, file);
+
+					String[] internetAddress = new String[emailTemplate.getToMailId().size()];
+					int i = 0;
+					for (String tomail : emailTemplate.getToMailId()) {
+						internetAddress[i] = tomail;
+						i++;
+					}
+					helper.setTo(internetAddress);
+					if (emailTemplate.getSubject() != null) {
+						helper.setSubject(emailTemplate.getSubject());
+					} else {
+						helper.setSubject(applicationProperties.getMailSubject());
+					}
+					helper.setFrom(emailHelper.getFromAddress());
+					helper.setText(emailTemplate.getBody(), true);
+					javaMailSender.send(msg);
+				} catch (MessagingException e) {
+
+					logger.error(e.getMessage(), e);
+					throw e;
+				} catch (Exception e) {
+
+					logger.error(e.getMessage(), e);
+				}
+				return "Mail initiated";
 			}
-			helper.setTo(internetAddress);
-			if (emailTemplate.getSubject() != null) {
-				helper.setSubject(emailTemplate.getSubject());
-			} else {
-				helper.setSubject(applicationProperties.getMailSubject());
-			}
-			helper.setFrom(emailHelper.getFromAddress());
-			helper.setText(emailTemplate.getBody(), true);
-			javaMailSender.send(msg);
-		} catch (MessagingException e) {
+		});
 
-			logger.error(e.getMessage(), e);
-			throw e;
-		} catch (Exception e) {
-
-			logger.error(e.getMessage(), e);
-		}
 	}
-
 }
