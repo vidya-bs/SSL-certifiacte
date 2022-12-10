a60b3f2749b5463e21b17e19e677669cce99b423
Yathin <yathin.somaiah@itorix.com>
2022-12-10 11:38:32 +0530

    Update encoding decoding.

--- marketing/src/main/java/com/itorix/apiwiz/marketing/serviceimpl/NewsServiceImpl.java
+++ marketing/src/main/java/com/itorix/apiwiz/marketing/serviceimpl/NewsServiceImpl.java
@@ -7,6 +7,7 @@ import com.itorix.apiwiz.marketing.news.model.News;
 import com.itorix.apiwiz.marketing.news.model.NewsFilterRequest;
 import com.itorix.apiwiz.marketing.news.model.NewsStatus;
 import com.itorix.apiwiz.marketing.service.NewsService;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,66 +15,68 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @CrossOrigin
 @RestController
 @Slf4j
 public class NewsServiceImpl implements NewsService {
 
-    @Autowired
-    NewsDao newsDao;
+  @Autowired
+  NewsDao newsDao;
 
-    @UnSecure(ignoreValidation = true)
-    @Override
-    public ResponseEntity<?> createNews(String apikey,News news) throws Exception {
-        News obj = newsDao.createNews(news);
-        if(obj!=null){
-            return new ResponseEntity<>(obj, HttpStatus.CREATED);
-        }
-        return new ResponseEntity<>(HttpStatus.CONFLICT);
+  @UnSecure(ignoreValidation = true)
+  @Override
+  public ResponseEntity<?> createNews(String apikey, News news) throws Exception {
+    News obj = newsDao.createNews(news);
+    if (obj != null) {
+      return new ResponseEntity<>(obj, HttpStatus.CREATED);
     }
+    return new ResponseEntity<>(HttpStatus.CONFLICT);
+  }
 
-    @UnSecure(ignoreValidation = true)
-    @Override
-    public ResponseEntity<?> updateNews(String apikey,News news, String newsId) throws Exception {
-        return newsDao.updateNews(news,newsId);
-    }
+  @UnSecure(ignoreValidation = true)
+  @Override
+  public ResponseEntity<?> updateNews(String apikey, News news, String newsId) throws Exception {
+    return newsDao.updateNews(news, newsId);
+  }
 
-    @UnSecure(ignoreValidation = true)
-    @Override
-    public ResponseEntity<?> changeStatus(String apikey,String newsId, NewsStatus status) throws Exception {
-            if (status.name().equals("DRAFT")) {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
-            return newsDao.changeStatus(newsId, status);
+  @UnSecure(ignoreValidation = true)
+  @Override
+  public ResponseEntity<?> changeStatus(String apikey, String newsId, NewsStatus status)
+      throws Exception {
+    if (status.name().equals("DRAFT")) {
+      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
+    return newsDao.changeStatus(newsId, status);
+  }
 
-    @UnSecure(ignoreValidation = true)
-    @Override
-    public ResponseEntity<?> fetchAllNews(String apikey,int offset, int pageSize,String status) throws Exception {
-        PaginatedResponse paginatedResponse = new PaginatedResponse();
-        paginatedResponse.setPagination(newsDao.getPagination(offset,pageSize));
-        paginatedResponse.setData(newsDao.fetchAllNews(offset,pageSize,status));
-        return new ResponseEntity<>(paginatedResponse,HttpStatus.OK);
-    }
+  @UnSecure(ignoreValidation = true)
+  @Override
+  public ResponseEntity<?> fetchAllNews(String apikey, int offset, int pageSize, String status)
+      throws Exception {
+    PaginatedResponse paginatedResponse = new PaginatedResponse();
+    paginatedResponse.setPagination(newsDao.getPagination(offset, pageSize));
+    paginatedResponse.setData(newsDao.fetchAllNews(offset, pageSize, status));
+    return new ResponseEntity<>(paginatedResponse, HttpStatus.OK);
+  }
 
-    @UnSecure(ignoreValidation = true)
-    @Override
-    public ResponseEntity<?> getDataByFilter(String apikey,int offset, int pageSize, String filter,
-        NewsFilterRequest newsFilterRequest) throws Exception {
-        List<News> paginatedData = newsDao.getDataByFilter(offset,pageSize,filter,newsFilterRequest.getFilterValue());
-        PaginatedResponse response = new PaginatedResponse();
-        response.setPagination(newsDao.getPagination(offset,pageSize));
-        response.setData(paginatedData);
-        return new ResponseEntity<>(response, HttpStatus.OK);
-    }
+  @UnSecure(ignoreValidation = true)
+  @Override
+  public ResponseEntity<?> getDataByFilter(String apikey, int offset, int pageSize, String filter,
+      NewsFilterRequest newsFilterRequest) throws Exception {
+    List<News> paginatedData = newsDao.getDataByFilter(offset, pageSize, filter,
+        newsFilterRequest != null ? newsFilterRequest.getFilterValue() : "");
+    PaginatedResponse response = new PaginatedResponse();
+    response.setPagination(newsDao.getPagination(offset, pageSize));
+    response.setData(paginatedData);
+    return new ResponseEntity<>(response, HttpStatus.OK);
+  }
 
-    @UnSecure(ignoreValidation = true)
-    @Override
-    public ResponseEntity<?> deleteNews(String apikey,String newsId) throws Exception {
-        if (newsDao.deleteNews(newsId) != null)
-            return ResponseEntity.noContent().build();
-        return ResponseEntity.notFound().build();
-    }
+  @UnSecure(ignoreValidation = true)
+  @Override
+  public ResponseEntity<?> deleteNews(String apikey, String newsId) throws Exception {
+      if (newsDao.deleteNews(newsId) != null) {
+          return ResponseEntity.noContent().build();
+      }
+    return ResponseEntity.notFound().build();
+  }
 }
