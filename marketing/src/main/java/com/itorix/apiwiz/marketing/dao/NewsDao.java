56fb6bbee6ab743f9d1c933ad83e68c39ab4ab32
Gautami0517 <111334289+Gautami0517@users.noreply.github.com>
2023-02-20 10:39:53 +0530

    sonar lint modification

--- marketing/src/main/java/com/itorix/apiwiz/marketing/dao/NewsDao.java
+++ marketing/src/main/java/com/itorix/apiwiz/marketing/dao/NewsDao.java
@@ -29,7 +29,7 @@ import java.util.stream.Collectors;
 public class NewsDao {
 
 
-    private  static final String DATE_FORMAT="dd-MM-yyyy";
+    private static final String DATE_FORMAT = "dd-MM-yyyy";
 
     @Qualifier("masterMongoTemplate")
     @Autowired
