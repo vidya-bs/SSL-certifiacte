e06982d0931ddea520475fab049f0f4a64351c90
divakarv <divakar.v@itorix.com>
2023-02-21 11:38:18 +0530

    added bean name

--- design-studio/src/main/java/com/itorix/apiwiz/design/studio/config/ScannerSqlLiteConfig.java
+++ design-studio/src/main/java/com/itorix/apiwiz/design/studio/config/ScannerSqlLiteConfig.java
@@ -1,24 +1,30 @@
 package com.itorix.apiwiz.design.studio.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration;
 import org.springframework.data.relational.core.dialect.AnsiDialect;
 import org.springframework.data.relational.core.dialect.Dialect;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.sqlite.SQLiteConfig;
 import org.sqlite.SQLiteDataSource;
 import org.sqlite.SQLiteOpenMode;
 
+import javax.sql.DataSource;
+
 
 @Configuration
-public class ScannerSqlLiteConfig extends AbstractJdbcConfiguration {
+public class ScannerSqlLiteConfig {
 
   @Value("${datasource.common.url}")
   private String url;
 
-  @Bean
+  @Bean(name = "complianceDataSource")
   public SQLiteDataSource dataSource() {
     SQLiteDataSource dataSource = new SQLiteDataSource();
     dataSource.setUrl(url);
@@ -29,9 +35,9 @@ public class ScannerSqlLiteConfig extends AbstractJdbcConfiguration {
     return dataSource;
   }
 
-  @Override
-  public Dialect jdbcDialect(NamedParameterJdbcOperations operations) {
-    return AnsiDialect.INSTANCE;
+  @Bean(name = "complianceJdbcTemplate")
+  public JdbcTemplate commonJdbcTemplate(@Qualifier("complianceDataSource") DataSource commonDataSource) {
+    return new JdbcTemplate(commonDataSource);
   }
 
 }
