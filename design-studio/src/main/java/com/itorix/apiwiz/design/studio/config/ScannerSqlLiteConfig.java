8eb4c6a7898f669acd0994919fd6c346a2711930
rahul <rahul.r@itorix.com>
2023-08-11 13:02:28 +0530

    - postgres trigger

--- design-studio/src/main/java/com/itorix/apiwiz/design/studio/config/ScannerSqlLiteConfig.java
+++ design-studio/src/main/java/com/itorix/apiwiz/design/studio/config/ScannerSqlLiteConfig.java
@@ -1,6 +1,5 @@
 package com.itorix.apiwiz.design.studio.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
